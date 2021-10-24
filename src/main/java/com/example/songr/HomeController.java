8c37aff24507aec3b5397a61ae4b59107182c9c9
AbdalrahmanAbualata <abuataabooood@yahoo.com>
2021-10-24 19:38:02 +0300

    lab 11

---
+++ src/main/java/com/example/songr/HomeController.java
@@ -0,0 +1,18 @@
+package com.example.songr;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class HomeController {
+
+//    http://localhost:8080
+    @GetMapping("/")
+    String splash(){return "splash";}
+
+
+    //     http://localhost:8080/hello
+    @GetMapping("/hello")  // short form of mapping end points
+    public String getRequestFunction(){
+        return "hello";
+    }
+}
--- src/test/java/com/example/songr/SongrApplicationTests.java
+++ src/test/java/com/example/songr/SongrApplicationTests.java
@@ -2,6 +2,7 @@ package com.example.songr;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class SongrApplicationTests {
@@ -9,5 +10,22 @@ class SongrApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	@Test
+	void testConstructorOfAlbumClass() {
+		Album album1 = new Album("erry-Jacks-Seasons-In-The-Sun", "abd ata",6,1440,"https://static.stereogum.com/uploads/2019/05/Terry-Jacks-Seasons-In-The-Sun-1557350323-520x520.jpg");
+		String output = album1.toString();
+		String expected = "Album{title='erry-Jacks-Seasons-In-The-Sun', artist='abd ata', songCount=6, length=1440, imageUrl='https://static.stereogum.com/uploads/2019/05/Terry-Jacks-Seasons-In-The-Sun-1557350323-520x520.jpg'}";
+		assertEquals(expected,output);
+	}
+
+	@Test
+	void testSetterAndGetterOfAlbumClass() {
+		Album album1 = new Album("erry-Jacks-Seasons-In-The-Sun", "abd ata",6,1440,"https://static.stereogum.com/uploads/2019/05/Terry-Jacks-Seasons-In-The-Sun-1557350323-520x520.jpg");
+		album1.setSongCount(9);
+		int output = album1.getSongCount();
+		int expected = 9;
+		assertEquals(expected,output);
+	}
+
 
 }
